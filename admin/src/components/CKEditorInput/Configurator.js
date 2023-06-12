9bd631895a1857e5a702ff82ca24b935fe0a5dc0
Luca Faccio <luca@sunnyday.software>
2023-06-13 01:08:32 +0200

    Added Source code editor to standard and rich versions

--- admin/src/components/CKEditorInput/Configurator.js
+++ admin/src/components/CKEditorInput/Configurator.js
@@ -1,30 +1,4 @@
-import ckeditor5Dll from "ckeditor5/build/ckeditor5-dll.js";
-
-import ckeditor5AlignmentDll from "@ckeditor/ckeditor5-alignment/build/alignment.js";
-import ckeditor5AutoformatDll from "@ckeditor/ckeditor5-autoformat/build/autoformat.js";
-import ckeditor5BasicStylesDll from "@ckeditor/ckeditor5-basic-styles/build/basic-styles.js";
-import ckeditor5BlockQuoteDll from "@ckeditor/ckeditor5-block-quote/build/block-quote.js";
-import ckeditor5CodeBlockDll from "@ckeditor/ckeditor5-code-block/build/code-block.js";
-import ckeditor5EssentialsDll from "@ckeditor/ckeditor5-essentials/build/essentials.js";
-import ckeditor5FontDll from "@ckeditor/ckeditor5-font/build/font.js";
-import ckeditor5HeadingDll from "@ckeditor/ckeditor5-heading/build/heading.js";
-import ckeditor5HighlightDll from '@ckeditor/ckeditor5-highlight/build/highlight.js';
-import ckeditor5HtmlEmbedDll from "@ckeditor/ckeditor5-html-embed/build/html-embed.js";
-import ckeditor5HorizontalLineDll from "@ckeditor/ckeditor5-horizontal-line/build/horizontal-line.js";
-import ckeditor5MarkdownDll from '@ckeditor/ckeditor5-markdown-gfm/build/markdown-gfm';
-import ckeditor5MediaEmbedDll from "@ckeditor/ckeditor5-media-embed/build/media-embed.js";
-import ckeditor5ImageDll from "@ckeditor/ckeditor5-image/build/image.js";
-import ckeditor5IndentDll from "@ckeditor/ckeditor5-indent/build/indent.js";
-import ckeditor5LinkDll from "@ckeditor/ckeditor5-link/build/link.js";
-import ckeditor5ListDll from "@ckeditor/ckeditor5-list/build/list.js";
-import ckeditor5PasteFromOfficeDll from "@ckeditor/ckeditor5-paste-from-office/build/paste-from-office.js";
-import ckeditor5FindAndReplaceDll from "@ckeditor/ckeditor5-find-and-replace/build/find-and-replace.js";
-import ckeditor5RemoveFormatDll from "@ckeditor/ckeditor5-remove-format/build/remove-format.js";
-import ckeditor5SpecialCharactersDll from "@ckeditor/ckeditor5-special-characters/build/special-characters.js";
-import ckeditor5TableDll from "@ckeditor/ckeditor5-table/build/table.js";
-import ckeditor5WordCountDll from "@ckeditor/ckeditor5-word-count/build/word-count.js";
-import ckeditor5MaximumLengthDll from "@reinmar/ckeditor5-maximum-length/build/maximum-length.js";
-import { StrapiMediaLib } from "./plugins/StrapiMediaLib";
+import {StrapiMediaLib} from "./plugins/StrapiMediaLib";
 
 const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
   light: {
@@ -64,11 +38,11 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
     ],
     heading: {
       options: [
-        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-        { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+        {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+        {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+        {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
+        {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
+        {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
       ]
     },
     image: {
@@ -118,26 +92,28 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
       window.CKEditor5.table.TableColumnResize,
       window.CKEditor5.table.TableCaption,
       window.CKEditor5.wordCount.WordCount,
+      window.CKEditor5.sourceEditing.SourceEditing,
+      window.CKEditor5.htmlSupport.GeneralHtmlSupport,
       StrapiMediaLib
     ],
     toolbar: [
-        'undo', 'redo',
-        '|',
-        'heading',
-        '|',
-        'bold', 'italic',
-        '|',
-        'link', 'strapiMediaLib', 'mediaEmbed', 'blockQuote', 'insertTable', 'codeBlock',
-        '|',
-        'bulletedList', 'numberedList', 'outdent', 'indent'
+      'undo', 'redo',
+      '|',
+      'heading', "sourceEditing",
+      '|',
+      'bold', 'italic',
+      '|',
+      'link', 'strapiMediaLib', 'mediaEmbed', 'blockQuote', 'insertTable', 'codeBlock',
+      '|',
+      'bulletedList', 'numberedList', 'outdent', 'indent'
     ],
     heading: {
       options: [
-        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-        { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+        {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+        {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+        {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
+        {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
+        {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
       ]
     },
     image: {
@@ -161,6 +137,35 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
         'toggleTableCaption'
       ]
     },
+    htmlSupport: {
+      allow: [
+        {
+          name: 'h3',
+          attributes: {
+            sizes: true,
+            loading: true,
+            class: true
+          }
+        },
+        {
+          name: 'p',
+          attributes: {
+            sizes: true,
+            loading: true,
+            class: true
+          }
+        },
+        {
+          name: 'span',
+          attributes: {
+            sizes: true,
+            loading: true,
+            class: true
+          }
+        },
+
+      ]
+    }
   },
 
   rich: {
@@ -212,6 +217,8 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
       window.CKEditor5.table.TableCaption,
       window.CKEditor5.wordCount.WordCount,
       window.CKEditor5.highlight.Highlight,
+      window.CKEditor5.sourceEditing.SourceEditing,
+      window.CKEditor5.htmlSupport.GeneralHtmlSupport,
       StrapiMediaLib
     ],
     toolbar: {
@@ -220,7 +227,7 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
         '|',
         'findAndReplace', 'selectAll',
         '|',
-        'heading',
+        'heading', "sourceEditing",
         '|',
         'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor',
         '|',
@@ -230,48 +237,48 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
         '|',
         'alignment',
         '|',
-        'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent', 
+        'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent',
       ],
       shouldNotGroupWhenFull: true
     },
     heading: {
       options: [
-        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-        { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+        {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+        {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+        {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
+        {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
+        {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
       ]
     },
     list: {
       properties: {
-          styles: true,
-          startIndex: true,
-          reversed: true
+        styles: true,
+        startIndex: true,
+        reversed: true
       }
     },
     image: {
       resizeUnit: "%",
-      resizeOptions: [ {
+      resizeOptions: [{
         name: 'resizeImage:original',
         value: null,
         icon: 'original'
       },
-      {
-        name: 'resizeImage:25',
-        value: '25',
-        icon: 'small'
-      },
-      {
-        name: 'resizeImage:50',
-        value: '50',
-        icon: 'medium'
-      },
-      {
-        name: 'resizeImage:75',
-        value: '75',
-        icon: 'large'
-      } ],
+        {
+          name: 'resizeImage:25',
+          value: '25',
+          icon: 'small'
+        },
+        {
+          name: 'resizeImage:50',
+          value: '50',
+          icon: 'medium'
+        },
+        {
+          name: 'resizeImage:75',
+          value: '75',
+          icon: 'large'
+        }],
       toolbar: [
         'imageStyle:inline', 'imageStyle:block', 'imageStyle:side',
         '|',
@@ -296,15 +303,15 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
     },
     fontSize: {
       options: [
-          9,
-          11,
-          13,
-          'default',
-          17,
-          19,
-          21,
-          27,
-          35,
+        9,
+        11,
+        13,
+        'default',
+        17,
+        19,
+        21,
+        27,
+        35,
       ],
       supportAllValues: false
     },
@@ -331,11 +338,40 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
       columns: 5,
       documentColors: 10,
     },
+    htmlSupport: {
+      allow: [
+        {
+          name: 'h3',
+          attributes: {
+            sizes: true,
+            loading: true,
+            class: true
+          }
+        },
+        {
+          name: 'p',
+          attributes: {
+            sizes: true,
+            loading: true,
+            class: true
+          }
+        },
+        {
+          name: 'span',
+          attributes: {
+            sizes: true,
+            loading: true,
+            class: true
+          }
+        },
+
+      ]
+    }
   }
 };
 
 export default class Configurator {
-  constructor ( fieldConfig ) {
+  constructor(fieldConfig) {
     this.fieldConfig = fieldConfig;
   }
 
@@ -345,12 +381,12 @@ export default class Configurator {
     const maxLength = this.fieldConfig.maxLength;
     const outputOption = this.fieldConfig.options.output;
 
-    if ( outputOption === 'Markdown' ) {
-      config.plugins.push( window.CKEditor5.markdownGfm.Markdown );
+    if (outputOption === 'Markdown') {
+      config.plugins.push(window.CKEditor5.markdownGfm.Markdown);
     }
 
-    if ( maxLength ) {
-      config.plugins.push( window.CKEditor5.maximumLength.MaximumLength );
+    if (maxLength) {
+      config.plugins.push(window.CKEditor5.maximumLength.MaximumLength);
 
       config.maximumLength = {
         characters: maxLength
@@ -363,7 +399,7 @@ export default class Configurator {
   _getBaseConfig() {
     const presetName = this.fieldConfig.options.preset;
 
-    switch ( presetName ) {
+    switch (presetName) {
       case 'light':
         return CKEDITOR_BASE_CONFIG_FOR_PRESETS.light;
       case 'standard':
