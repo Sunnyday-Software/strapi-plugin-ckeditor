316f48a7ab6462e80a66eea9a7ed57ef4773b0c8
Luca Faccio <luca@sunnyday.software>
2023-06-14 22:21:47 +0200

    Implemented heading styles with tailwind classes

--- admin/src/components/CKEditorInput/Configurator.js
+++ admin/src/components/CKEditorInput/Configurator.js
@@ -66,11 +66,11 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
     ],
     heading: {
       options: [
-        {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
-        {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
-        {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
-        {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
-        {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
+        {model: 'paragraph', view: {name: 'p', classes: "text-current"}, title: 'Paragraph', class: 'ck-heading_paragraph'},
+        {model: 'heading1', view: {name: 'h3', classes: "text-4xl"}, title: 'Heading 1', class: 'ck-heading_heading1'},
+        {model: 'heading2', view: {name: 'h4', classes: "text-3xl"}, title: 'Heading 2', class: 'ck-heading_heading2'},
+        {model: 'heading3', view: {name: 'h5', classes: "text-2xl"}, title: 'Heading 3', class: 'ck-heading_heading3'},
+        {model: 'heading4', view: {name: 'h6', classes: "text-xl"}, title: 'Heading 4', class: 'ck-heading_heading4'},
       ]
     },
     image: {
@@ -137,11 +137,11 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
     ],
     heading: {
       options: [
-        {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
-        {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
-        {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
-        {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
-        {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
+        {model: 'paragraph', view: {name: 'p', classes: "text-current"}, title: 'Paragraph', class: 'ck-heading_paragraph'},
+        {model: 'heading1', view: {name: 'h3', classes: "text-4xl"}, title: 'Heading 1', class: 'ck-heading_heading1'},
+        {model: 'heading2', view: {name: 'h4', classes: "text-3xl"}, title: 'Heading 2', class: 'ck-heading_heading2'},
+        {model: 'heading3', view: {name: 'h5', classes: "text-2xl"}, title: 'Heading 3', class: 'ck-heading_heading3'},
+        {model: 'heading4', view: {name: 'h6', classes: "text-xl"}, title: 'Heading 4', class: 'ck-heading_heading4'},
       ]
     },
     image: {
@@ -271,11 +271,11 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
     },
     heading: {
       options: [
-        {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
-        {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
-        {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
-        {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
-        {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
+        {model: 'paragraph', view: {name: 'p', classes: "text-current"}, title: 'Paragraph', class: 'ck-heading_paragraph'},
+        {model: 'heading1', view: {name: 'h3', classes: "text-4xl"}, title: 'Heading 1', class: 'ck-heading_heading1'},
+        {model: 'heading2', view: {name: 'h4', classes: "text-3xl"}, title: 'Heading 2', class: 'ck-heading_heading2'},
+        {model: 'heading3', view: {name: 'h5', classes: "text-2xl"}, title: 'Heading 3', class: 'ck-heading_heading3'},
+        {model: 'heading4', view: {name: 'h6', classes: "text-xl"}, title: 'Heading 4', class: 'ck-heading_heading4'},
       ]
     },
     list: {
